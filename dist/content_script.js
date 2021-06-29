0909c687d9408d7505b38c17b4b72985d45a35c9
Jaidon-Smith <jsjsrobert500@gmail.com>
2021-06-29 23:34:41 +1000

    edited version

--- dist/content_script.js
+++ dist/content_script.js
@@ -180,6 +180,7 @@ scriptElem.text = `
     if (downloadButtonElem) {
       downloadButtonElem.style.color = 'black';
       downloadButtonElem.disabled = false;
+      downloadSRT();
     }
   }
 
